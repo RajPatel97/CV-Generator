ab42e06227bee692af9798e6fee8c6a821d4de6a
Raj Patel <raj191@live.com>
2021-07-30 17:09:20 -0400

    adding some new things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
+import GeneralInformation from "./components/General";
 function App() {
   return (
     <div className="body-content">
       <Header />
       <div className="input-container">
-        hello this is raj
+      <GeneralInformation />
       </div>
 
       
--- src/components/General.js
+++ src/components/General.js
@@ -1,6 +1,15 @@
 const GeneralInformation = () => {
     return (
-      <h2 className="title">Personal Information</h2>
+      <div>
+        <h2 className="title">Personal Information</h2>
+        <input type="text" placeholder = "First Name" class = "inputStyle"/>
+        <input type="text" placeholder = "Last Name" class = "inputStyle"/>
+        <input type="text" placeholder = "Address" class = "inputStyle"/>
+        <input type="text" placeholder = "Phone Number" class = "inputStyle"/>
+        <input type="text" placeholder = "Email" class = "inputStyle"/>
+        <input type="text" placeholder = "Description" class = "description-inputStyle" class = "inputStyle"/>
+
+      </div>
       );
 }
  
