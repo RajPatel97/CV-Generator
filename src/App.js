f74f654f29180cc080f2fd541bd5bdfb0c6b97af
Raj Patel <raj191@live.com>
2021-07-30 21:58:04 -0400

    fixing display flex issues

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import Header from "./components/Header";
 import GeneralInformation from "./components/General";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
+import CVdisplay from "./components/CVdisplay";
 function App() {
   return (
+    <div>
+    <Header />
     <div className="body-content">
-      <Header />
       <div className="input-container">
       <GeneralInformation />
       <Education />
       <Experience />
       </div>
-
-      
+      {/* add logic for the cv display here*/}
+      <CVdisplay />
     </div>
-    
+    </div>
   );
 }
 
---
+++ src/components/CVdisplay.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+
+
+const CVdisplay = () => {
+    return (
+        <div className="input-container">
+        <header className="displayHeader">This is the header</header>
+
+
+        </div>
+      );
+}
+ 
+export default CVdisplay;
