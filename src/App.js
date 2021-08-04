f6be09be82afe43937eb099f43ed4d9c024a2191
RajPatel <raj191@live.com>
2021-08-04 15:54:11 -0400

    adding a footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
@@ -8,6 +9,7 @@ function App() {
     <Form />
       
     </div>
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return ( 
+        <div className="footer-wrapper">
+            <h3 className="footer-name">Created by Raj Patel</h3>
+            <h3 className="footer-github">Github</h3>
+        </div>
+     );
+}
+ 
+export default Footer;
